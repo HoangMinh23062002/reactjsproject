528dc2e6f47d7e170c35e6d0346e4f01ef076f02
HoangMinh23062002 <minh.hoang23@student.passerellesnumeriques.org>
2022-06-17 15:42:41 +0700

    doing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import './App.css'
-import Comleft from './components/component_left'
+import Component_left from './components/component_left'
 import Comright from './components/component_right'
+import Req_content from './components/requir_content'
+import Contact from './components/contact'
 
 class App extends Component {
   render() {
     return (
       <div className="Overall-component">
-          <div>
-          <Comleft/>
+          <div className="Both-side">
+            <Component_left/>
+            <Req_content/>
           </div>
           <div>
           <Comright/>
+          </div>
+          <div>
+        <Contact/>
           </div>
       </div>
     );
